b3f15a6b570be604f11785eabd889e1705085a76
eiNasota <jhonysouza017@gmail.com>
2020-09-15 09:29:29 -0300

    Att

--- CCI/IMC/app.js
+++ CCI/IMC/app.js
@@ -1,10 +1,5 @@
 function imc() {
     let peso = parseFloat(document.getElementById('peso').value)
     let altura = parseFloat(document.getElementById('altura').value)
-    if (peso === NaN) {
-        return alert('Insira um peso valida')
-    } if (altura === NaN){
-        return alert('Insira uma altura valida')
-    }
     console.log(peso, altura)
 }
